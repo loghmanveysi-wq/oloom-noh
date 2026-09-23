9f3a7626524950ae788e970f0d5dc61674760019
loghmanveysi-wq <loghman.veysi@gmail.com>
2026-09-23 18:16:34 +0330

    Initialize Flutter app with Firebase and Hive

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'theme/app_theme.dart';
+import 'services/theme_provider.dart';
+import 'screens/home_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await Hive.initFlutter();
+  await Hive.openBox('settingsBox');
+  await Hive.openBox('progressBox');
+  await Hive.openBox('offlinePdfBox');
+  runApp(const OloomNohApp());
+}
+
+class OloomNohApp extends StatelessWidget {
+  const OloomNohApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, _) {
+          return MaterialApp(
+            title: 'علوم نهم – استاد ویسی',
+            debugShowCheckedModeBanner: false,
+            locale: const Locale('fa', 'IR'),
+            supportedLocales: const [Locale('fa', 'IR')],
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            themeMode: themeProvider.themeMode,
+            builder: (context, child) {
+              return Directionality(
+                textDirection: TextDirection.rtl,
+                child: child!,
+              );
+            },
+            home: const HomeScreen(),
+          );
+        },
+      ),
+    );
+  }
+}
